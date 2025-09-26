5ac127e325f1e6acdb27b646abbad0392b0afb72
Gregory Herschlag <gjh@math.duke.edu>
2025-09-25 22:53:34 -0400

    upate with bug fixes

--- src/chains/annealed_importance_sampling.jl
+++ src/chains/annealed_importance_sampling.jl
@@ -6,12 +6,13 @@ function track_weight_and_modify_measure!(
     measure::Measure,
     modify_measure!::Function
 )
-    println("weight before", weight.value)
+    # println("weight before", weight.value)
     e1 = get_energy(partition, measure)
     modify_measure!(measure, cur_step, total_steps)
     e2 = get_energy(partition, measure)
     weight.value += e2-e1
-    println("weight after", weight.value)
+    # println("weiweight.value)
+    @show weight.value, cur_step, total_steps, values(measure.weights), e1, e2
 end
 
 function run_annealed_importance_sampling!(
@@ -36,15 +37,17 @@ function run_annealed_importance_sampling!(
                                  base_steps_per_sample, rng)
         partition_to_anneal = deepcopy(partition)
         log_weight = MutableFloat(0.0)
+        @show "starting annealing"
+        modify_measure!(measure, 0, 1)
         run_metropolis_hastings!(partition_to_anneal, proposal, measure, 
                                  steps_per_annealing, rng; writer=writer,
-                                 output_freq=steps_per_annealing,
+                                 output_freq=steps_per_annealing*2,
                                  output_initial=false,
                                  prestepf=track_weight_and_modify_measure!,
-                                 prestepargs=(partition, total_steps, 
+                                 prestepargs=(partition, steps_per_annealing, 
                                               log_weight, measure, 
                                               modify_measure!),
                                  weight=log_weight)
-        break
+        # break
     end
 end
